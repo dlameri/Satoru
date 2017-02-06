31a4b5c3d07ca286eb08b15f90369023b2051a7d
Dimitri Lameri <dimitrilameri@gmail.com>
2017-02-06 00:05:17 -0200

    Rodando o initializer de producao

--- Satoru/src/main/java/com/satoru/config/initializer/ProductionDataInitializer.java
+++ Satoru/src/main/java/com/satoru/config/initializer/ProductionDataInitializer.java
@@ -1,7 +1,12 @@
 package com.satoru.config.initializer;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
 import com.satoru.domain.Role;
 
+@Component
+@Profile(value="production")
 public class ProductionDataInitializer extends DataInitializer{
 
 	@Override
